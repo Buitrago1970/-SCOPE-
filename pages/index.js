add7679db840a726439dd3001635bee95f577426
RogelioRichmanAstronaut <56854119+RogelioRichmanAstronaut@users.noreply.github.com>
2022-05-03 12:18:53 -0500

    styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
+import styled from 'styled-components'
 
-import styles from '../styles/Home.module.css'
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`
 
 export default function Home() {
-  return (
-    <div className='container-page'>
-      <h1>scope 😇🥰😋😚😙😌🥰🥳</h1>
-    </div>
-  )
+  return <Title>My page</Title>
 }
-
