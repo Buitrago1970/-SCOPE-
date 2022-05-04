d77150b423e0ecc4af9c9553e1078cfd00e9566b
RogelioRichmanAstronaut <56854119+RogelioRichmanAstronaut@users.noreply.github.com>
2022-05-04 12:08:30 -0500

    fonts

---
+++ components/0Products/StyledCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledCard = ({id, limited, tallas,nombre,img,precio}) => {
+  return (
+    <div>{nombre}</div>
+  )
+}
+
+export default StyledCard
---
+++ datos/ProductosDatos.js
@@ -0,0 +1,99 @@
+export const PortafolioObjUno = {
+    id: "1",
+    limited: false,
+    tallas: ["XL","XS","S"],
+    nombre: "T-SHIRT",
+    img: require("../public/productos/t-shirt.png").default,
+    alt: "scope t-shirt",
+    precio: "100000",
+    hrefLink:"/", 
+    ariaLabel:""
+  };
+export const PortafolioObjDos = {
+    id: "2",
+    limited: true,
+    tallas: ["XL","XS","S"],
+    nombre: "T-SHIRT2",
+    img: require("../public/productos/t-shirt2.png").default,
+    alt: "scope t-shirt",
+    precio: "510000",
+    hrefLink:"/", 
+    ariaLabel:""
+  };
+export const PortafolioObjTres = {
+    id: "3",
+    limited: false,
+    tallas: ["XL","XS","S"],
+    nombre: "CAP",
+    img: require("../public/productos/cap.png").default,
+    alt: "cap",
+    precio: "56000",
+    hrefLink:"/", 
+    ariaLabel:""
+  };
+export const PortafolioObjCuatro = {
+    id: "4",
+    limited: false,
+    tallas: ["XL","XS","S"],
+    nombre: "JACKET",
+    img: require("../public/productos/jacket.png").default,
+    alt: "jacket",
+    precio: "875000",
+    hrefLink:"/", 
+    ariaLabel:""
+  };
+export const PortafolioObjCinco = {
+    id: "5",
+    limited: false,
+    tallas: ["XL","XS","S"],
+    nombre: "GABBAN",
+    img: require("../public/productos/gaban.png").default,
+    alt: "gaban",
+    precio: "690000",
+    hrefLink:"/", 
+    ariaLabel:""
+  };
+export const PortafolioObjSeis = {
+    id: "6",
+    limited: false,
+    tallas: ["XL","XS","S"],
+    nombre: "T-SHIRT3",
+    img: require("../public/productos/t-shirt3.png").default,
+    alt: "gaban",
+    precio: "40000",
+    hrefLink:"/", 
+    ariaLabel:""
+  };
+export const PortafolioObjSiete = {
+    id: "7",
+    limited: false,
+    tallas: ["XL","XS","S"],
+    nombre: "T-SHIRT4",
+    img: require("../public/productos/t-shirt4.png").default,
+    alt: "gaban",
+    precio: "94000",
+    hrefLink:"/", 
+    ariaLabel:""
+  };
+export const PortafolioObjOcho = {
+    id: "8",
+    limited: false,
+    tallas: ["XL","XS","S"],
+    nombre: "DRESS",
+    img: require("../public/productos/vestido2.png").default,
+    alt: "gaban",
+    precio: "460000",
+    hrefLink:"/", 
+    ariaLabel:""
+  };
+  export const PortafolioObjNueve = {
+    id: "9",
+    limited: true,
+    tallas: ["XL","XS","S"],
+    nombre: "DRESS red",
+    img: require("../public/productos/vestido-rojo.png").default,
+    alt: "gaban",
+    precio: "950000",
+    hrefLink:"/", 
+    ariaLabel:""
+  };
